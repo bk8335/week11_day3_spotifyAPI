bcc81145912f8884c1619fd76bfcd508d6248ceb
Benjamin <benjamin.kinnard1@gmail.com>
2017-05-17 22:25:38 +0100

    included links to spotify album page

--- public/app.js
+++ public/app.js
@@ -28,19 +28,41 @@ var makeRequest = function(url , callback) {
 
 var populateAlbumList = function(albums) {
 
-  console.log(albums.albums.items[0].name)
-  console.log(albums.albums.items[1].name)
+  // console.log(albums.albums.items[0].images[0].url)
 
-  console.log(albums.albums.items.length)
+  // console.log(albums.albums.items[0].artists[0].name)
 
+  console.log(albums.albums.items[0].external_urls.spotify)
 
-  var ol = document.getElementById("albums");
+  var ul = document.getElementById("all-info")
+  var li1 = document.getElementById("artists");
+  var li2 = document.getElementById("album-name");
+  var li3 = document.getElementById("album-image");
 
   for(i=0; i<albums.albums.items.length; i++){
-    var li = document.createElement('li');
-    li.innerText = albums.albums.items[i].name;
-    ol.appendChild(li);
+    var li1 = document.createElement('li');
+    li1.innerText = albums.albums.items[i].artists[0].name;
+    var li2 = document.createElement('li');
+    li2.innerHTML = "<a href = " + albums.albums.items[i].external_urls.spotify + ">" + albums.albums.items[i].name; + "</a"
+    var li3 = document.createElement('li');
+    li3.innerHTML = "<img src=" + albums.albums.items[i].images[0].url + ">"
+    var li4 = document.createElement('li');
+    li4.innerText = "............................................................"
+
+    
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+    ul.appendChild(li4);
   }
+
+  // for(i=0; i<albums.albums.items.length; i++){
+  //   var li2 = document.createElement('li');
+  //   li2.innerText = albums.albums.items[i].name;
+  //   ul.appendChild(li2);
+  // }
+
+  
 
 
   // for(item in albums.albums.items){
